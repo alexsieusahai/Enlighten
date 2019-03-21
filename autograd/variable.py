897c6445eafa776c1cceb75583a5b99b4530fe11
alexsieusahai <asieusah@ualberta.ca>
2019-03-21 11:25:36 -0600

    Merge variable and node classes

--- autograd/variable.py
+++ autograd/variable.py
@@ -1,51 +1,53 @@
-from variable import Variable
 from primitives import Add, Multiply, Divide, Exponent
 
 
-class Node:
-    def __init__(self, parent0, parent1, primitive, eager=True):
-        self.parent0 = self.ensure_node(parent0)
-        self.parent1 = self.ensure_node(parent1)
+class Variable:
+    def __init__(self, parent0, parent1=None, primitive=None, eager=True):
+        is_variable = parent1 is None
+        if not is_variable:
+            self.parent0 = self.ensure_node(parent0)
+            self.parent1 = self.ensure_node(parent1)
         self.primitive = primitive
-        self.value = None
+        self.value = parent0 if is_variable else None
+        self.grad_dict = {id(self): 1}
         if eager:
             self.value = self.compute()
             
     def __str__(self):
         return str(self.value)
-        
-    def ensure_node(self, node):
-        if isinstance(node, Node) or isinstance(node, Variable):
-            return node
-        return Variable(node)
     
     def __neg__(self):
-        return Node(self, -1, Multiply())
+        return Variable(self, -1, Multiply())
         
     def __add__(self, node):
         node = self.ensure_node(node)
-        return Node(self, node, Add())
+        return Variable(self, node, Add())
     
     def __radd__(self, node):
-        return Node(node, self, Add())
+        return Variable(node, self, Add())
     
     def __mul__(self, node):
-        return Node(self, node, Multiply())
+        return Variable(self, node, Multiply())
     
     def __rmul__(self, node):
-        return Node(node, self, Multiply())
+        return Variable(node, self, Multiply())
     
     def __truediv__(self, node):
-        return Node(self, node, Divide())
+        return Variable(self, node, Divide())
     
     def __rtruediv__(self, node):
-        return Node(node, self, Divide())
+        return Variable(node, self, Divide())
     
     def __pow__(self, node):
-        return Node(self, node, Exponent())
+        return Variable(self, node, Exponent())
     
     def __rpow__(self, node):
-        return Node(node, self, Exponent())
+        return Variable(node, self, Exponent())
+        
+    def ensure_node(self, node):
+        if isinstance(node, Variable):
+            return node
+        return Variable(node)
     
     def compute(self):
         if self.value is None:
@@ -64,9 +66,9 @@ class Node:
         for key in self.parent1.grad_dict:
             self.grad_dict[key] = self.parent1.grad_dict[key] * parent1_grad
             
-    def get_grad(self, var: Variable):
+    def get_grad(self, var):
         """
-        Returns the gradient of Node with respect to variable, using the 
+        Returns the gradient of Variable with respect to variable, using the 
             position of the variable passed in in memory.
         """
         return self.grad_dict[id(var)]
