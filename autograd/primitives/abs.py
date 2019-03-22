f37363518ccdb32d2196ba97f08c641d6c3c1ffc
alexsieusahai <alexsieu14@gmail.com>
2019-03-21 23:17:05 -0600

    Fix abs primitive

--- autograd/primitives/abs.py
+++ autograd/primitives/abs.py
@@ -1,13 +1,13 @@
 class Abs:
     def __init__(self):
-        self.f = lambda x: x if x.value >= 0 else -1 * x
+        self.f = lambda x: x if x >= 0 else -1 * x
 
-    def __call__(self, x):
+    def __call__(self, x, y=None):
         return self.f(x)
 
-    def get_grad(self, x):
+    def get_grad(self, x, y):
         """
         Not the true gradient of abs (not defined at 0)
             but this should work anyways.
         """
-        return 1 if x.value >= 0 else -1
+        return (1 if x >= 0 else -1, None)
