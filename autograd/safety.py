c979877f2ccceae8e1313c0531b9c61d0c6f87eb
alexsieusahai <alexsieu14@gmail.com>
2019-03-21 23:16:35 -0600

    Add hadamard safety check

--- autograd/safety.py
+++ autograd/safety.py
@@ -7,15 +7,21 @@ def ensure_mul(mat0, mat1):
                 {len(mat0.entries[0])} != {len(mat1.entries)}')
         raise TypeError
 
-def ensure_add(mat0, mat1):
+def ensure_same_size(mat0, mat1, operation_name):
     """
     Ensures that mat0 and mat1 are the proper shapes for addition.
     """
     if len(mat0) != len(mat1):
-        print(f'Matrix sizes do not agree for addition in dimension 0: \n \
+        print(f'Matrix sizes are not same size for {operation_name} in dimension 0: \n \
                 {len(mat0)} != {len(mat1)}')
         raise TypeError
     if len(mat0[0]) != len(mat1[0]):
-        print(f'Matrix sizes do not agree for addition in dimension 1: \n \
+        print(f'Matrix sizes are not same size for {operation_name} in  dimension 1: \n \
                 {len(mat0[0])} != {len(mat1[0])}')
         raise TypeError
+
+def ensure_add(mat0, mat1):
+    ensure_same_size(mat0, mat1, 'add')
+
+def ensure_hadamard(mat0, mat1):
+    ensure_same_size(mat0, mat1, 'hadamard')
