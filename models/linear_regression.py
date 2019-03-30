724a6ab4caa4a1c359c2dbae3a04f803724c5c4d
alexsieusahai <alexsieu14@gmail.com>
2019-03-30 15:50:42 -0600

    Add bias term to linear regression

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -14,19 +14,20 @@ class LinearRegression:
     That is, we assume that the manifold of the data is a plane, which we can model
         by a linear combination of the values of each variable.
     """
-    def __init__(self, params=None):
+    def __init__(self, params=None, bias=None):
         """
         :param params: The starting parameters to use.
         """
         self.params = params
+        self.bias = bias
 
 
-    def _evaluate(self, row: List[Variable]) -> Variable:
+    def _evaluate(self, row: Matrix) -> Matrix:
         """
         Using self.params, evaluates row, which we assume to be an iterable
             filled with Variable objects.
         """
-        return row * self.params
+        return row * self.params + self.bias
 
 
     def fit(self, loader: DataLoader, optimizer=None, loss_function=None) -> None:
@@ -46,26 +47,30 @@ class LinearRegression:
             # should import it from some loss functions directory or something in the future
             loss_function = lambda x, y: (x-y).abs()
 
-        if self.params is None:
-            self.params = Matrix([[Variable(random.random())] for _ in range(len(X[0]))])
+        for X, y in loader:
+            if self.params is None:
+                self.params = Matrix([[Variable(random.random())] for _ in range(len(X[0]))])
+                self.bias = Matrix([[Variable(random.random())]])
 
-        # without minibatching, I treat this is matrix mult; I should switch to matrix mult,
-        #     more general
-        for X_batch, y_batch in loader:
-            output = self._evaluate(X_batch)
-            loss = loss_function(output, y_batch)
+            output = self._evaluate(X)
+            loss = loss_function(output, y)
             self.params = optimizer.step(self.params, loss.get_grad(self.params))
+            self.bias = optimizer.step(self.bias, loss.get_grad(self.bias))
 
 
     def predict(self, X: Matrix) -> Matrix:
         """
         Given X, returns a list of predictions for X.
         """
-        return self._evaluate(X)
+        preds = []
+        for row in X:
+            preds.append(self._evaluate(Matrix([row]))[0])
+        return Matrix(preds)
+
 
 if __name__ == "__main__":
     true_params = [0.3, 3, 5.4]
-    f = lambda row: sum([row[i] * true_params[i] for i in range(len(row))])
+    f = lambda row: sum([row[i] * true_params[i] for i in range(len(row))]) + 3
     X = []
     y = []
     for _ in range(5000):
@@ -75,8 +80,10 @@ if __name__ == "__main__":
     loader = DataLoader(X, y)
 
     linreg = LinearRegression()
+    optim = SGD(0.01, minibatch_size=5)
     linreg.fit(loader)
     print(linreg.params)
+    print(linreg.bias)
 
     X_test = []
     for _ in range(10):
