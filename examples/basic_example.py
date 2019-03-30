940586134bd5c560562227a6d896142554d286cc
alexsieusahai <alexsieu14@gmail.com>
2019-03-30 02:38:44 -0600

    Update basic example

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -2,15 +2,15 @@ if __name__ == "__main__":
     import sys
     sys.path.append('..')
     from autograd import Variable, Matrix, zeros
-    from activation_functions import sigmoid
+    from activation_functions import sigmoid, relu
 
-    W0 = zeros(200, 2)
+    W0 = zeros(500, 2)
     W0.init_normal()
 
-    b0 = zeros(200, 1)
+    b0 = zeros(500, 1)
     b0.init_normal()
 
-    W1 = zeros(1, 200)
+    W1 = zeros(1, 500)
     W1.init_normal()
 
     b1 = zeros(1, 1)
@@ -21,8 +21,8 @@ if __name__ == "__main__":
             [Variable(2)]
         ])
 
-    hidden = sigmoid(W0 * x + b0)
-    output = sigmoid(W1 * hidden + b1)
+    hidden = relu(W0 * x + b0)
+    output = relu(W1 * hidden + b1)
     print(output)
     print(output.get_grad(W0))
     print(output.get_grad(b0))
