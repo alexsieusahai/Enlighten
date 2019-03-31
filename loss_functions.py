3fb48f3d8a70f137d27f868d2fdc6abeabfc6c04
alexsieusahai <alexsieu14@gmail.com>
2019-03-30 19:00:06 -0600

    Add binary cross entropy

--- loss_functions.py
+++ loss_functions.py
@@ -7,12 +7,13 @@ def mean_squared_error(x, y):
 def mean_absolute_error(x, y):
     return (x - y).abs().mean()
 
-def cross_entropy(x, y):
+def binary_cross_entropy(pred, target):
     """
-    Note that x and y must be the same shape.
+    This assumes all classes are either 0 or 1.
+    If more than 1 sample is passed in, the mean is taken.
     """
-    ones = x.initialize_matrix(1)
-    return -1 * (y.hadamard(x.log()) + (ones-y).hadamard((ones-x).log())).sum()
+    ones = pred.initialize_matrix(1)
+    return -1 * (target.hadamard(pred.log()) + (ones-target).hadamard((ones - pred).log())).mean()
 
 if __name__ == "__main__":
     x = Matrix([
@@ -22,13 +23,11 @@ if __name__ == "__main__":
         ])
 
     y = Matrix([
-        [Variable(0.5)],
-        [Variable(0.1)],
-        [Variable(0.4)]
+        [Variable(0)],
+        [Variable(1)],
+        [Variable(0)]
         ])
-
-    x - y
 
     print(mean_squared_error(x, y))
     print(mean_absolute_error(x, y))
-    print(cross_entropy(x, y))
+    print(binary_cross_entropy(x, y))
