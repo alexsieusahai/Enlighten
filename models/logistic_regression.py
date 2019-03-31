c71ccb1155544782bcda9d824daeb476f45bbc9f
alexsieusahai <alexsieu14@gmail.com>
2019-03-30 19:00:26 -0600

    Get logistic regression working with binary cross entropy

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -29,31 +29,39 @@ class LogisticRegression(LinearRegression):
         Uses the _evaluate function in LinearRegression, then applies a sigmoid function
             elementwise to the resulting matrix.
         """
-        mat = super()._evaluate(row)
-        return mat.elementwise_apply(sigmoid)
+        mat = super()._evaluate(row).elementwise_apply(sigmoid)
+        return mat
 
 
 if __name__ == "__main__":
     import random
-    true_params = [0.3, 0.7, 0.2]
-    f = lambda row: sigmoid(sum([row[i] * true_params[i] for i in range(len(row))]) + 0.3)
+
+    from loss_functions import binary_cross_entropy
+
+    params = [0.3, 0.6, 0.2]
+    f = lambda row: sigmoid(sum([row[i] * params[i] for i in range(len(row))]) + 0.2)
     X = []
     y = []
+    thresh = 0.3
     for _ in range(2000):
         X.append([random.random() for _ in range(3)])
-        y.append([f(X[-1])])
+        val = f(X[-1])
+        print(val)
+        y.append([val > thresh])
 
     loader = DataLoader(X, y)
 
-    linreg = LogisticRegression()
+    logreg = LogisticRegression()
     optim = SGD(0.01, minibatch_size=5)
-    linreg.fit(loader)
-    print(linreg.params)
-    print(linreg.bias)
+    logreg.fit(loader)
+    print(logreg.params)
+    print(logreg.bias)
+    print(binary_cross_entropy(logreg.predict(Matrix(X)), Matrix(y)))
 
     X_test = []
     for _ in range(10):
         X_test.append([random.random() for _ in range(3)])
 
-    outputs = linreg.predict(Matrix(X_test))
-    print(outputs)
+    outputs = logreg.predict(Matrix(X_test))
+    for i, output in enumerate(outputs):
+        print(output, f(X_test[i]) > thresh)
