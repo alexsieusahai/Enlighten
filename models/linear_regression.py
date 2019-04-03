f5889d48e2fe5fb5299548dbbf5929972626acaf
alexsieusahai <alexsieu14@gmail.com>
2019-04-03 09:24:36 -0600

    Add regularization

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -6,6 +6,7 @@ sys.path.append('..')
 from autograd import Variable, Matrix
 from optimizers import SGD
 from dataloader import DataLoader
+from loss_functions import mean_squared_error
 
 
 class LinearRegression:
@@ -14,13 +15,17 @@ class LinearRegression:
     That is, we assume that the manifold of the data is a plane, which we can model
         by a linear combination of the values of each variable.
     """
-    def __init__(self, params=None, bias=None):
+    def __init__(self, params=None, bias=None, alpha=0, beta=0):
         """
         :param params: The starting parameters to use.
+        :param bias: The starting bias to use.
+        :param alpha: The amount to scale the sum of L1 norms in the regularizing term.
+        :param beta: The amount to scale the sum of L2 norms in the regularizing term.
         """
         self.params = params
         self.bias = bias
-
+        self.alpha = alpha
+        self.beta = beta
 
     def _evaluate(self, row: Matrix) -> Matrix:
         """
@@ -28,7 +33,13 @@ class LinearRegression:
             filled with Variable objects.
         """
         return row * self.params + self.bias
+        
 
+    def _regularize(self) -> Matrix:
+        """
+        Returns the sum of the l1 and l2 norms of the parameters, scaled by alpha and beta.
+        """
+        return Matrix([[self.alpha]]) * self.params.sum() + self.beta * (self.params**2).sum()
 
     def fit(self, loader: DataLoader, optimizer=None, loss_function=None) -> None:
         """
@@ -44,8 +55,7 @@ class LinearRegression:
             optimizer = SGD(0.01)
 
         if loss_function is None:
-            # should import it from some loss functions directory or something in the future
-            loss_function = lambda x, y: (x-y).abs()
+            loss_function = mean_squared_error
 
         for X, y in loader:
             if self.params is None:
@@ -54,9 +64,9 @@ class LinearRegression:
 
             output = self._evaluate(X)
             loss = loss_function(output, y)
+            loss += self._regularize()
             self.params = optimizer.step(self.params, loss.get_grad(self.params))
             self.bias = optimizer.step(self.bias, loss.get_grad(self.bias))
-
 
     def predict(self, X: Matrix) -> Matrix:
         """
@@ -73,7 +83,7 @@ if __name__ == "__main__":
     f = lambda row: sum([row[i] * true_params[i] for i in range(len(row))]) + 3
     X = []
     y = []
-    for _ in range(5000):
+    for _ in range(5000): 
         X.append([random.random() for _ in range(3)])
         y.append([f(X[-1])])
 
