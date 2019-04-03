7ec399b6d80e46d5f99d24335aabe10eb16aba9d
alexsieusahai <alexsieu14@gmail.com>
2019-04-03 10:00:14 -0600

    Seperate models into their types

---
+++ linear_models/linear_regression.py
@@ -0,0 +1,79 @@
+import random
+from typing import List
+
+import os
+print(os.getcwd())
+import sys
+sys.path.append('.')
+from autograd import Variable, Matrix
+from loss_functions import mean_squared_error
+
+
+class LinearRegression:
+    """
+    Implements linear regression.
+    That is, we assume that the manifold of the data is a plane, which we can model
+        by a linear combination of the values of each variable.
+    """
+    def __init__(self, params=None, bias=None, alpha=0, beta=0):
+        """
+        :param params: The starting parameters to use.
+        :param bias: The starting bias to use.
+        :param alpha: The amount to scale the sum of L1 norms in the regularizing term.
+        :param beta: The amount to scale the sum of L2 norms in the regularizing term.
+        """
+        self.params = params
+        self.bias = bias
+        self.alpha = alpha
+        self.beta = beta
+
+    def _evaluate(self, row: Matrix) -> Matrix:
+        """
+        Using self.params, evaluates row, which we assume to be an iterable
+            filled with Variable objects.
+        """
+        return row * self.params + self.bias
+        
+
+    def _regularize(self) -> Matrix:
+        """
+        Returns the sum of the l1 and l2 norms of the parameters, scaled by alpha and beta.
+        """
+        return Matrix([[self.alpha]]) * self.params.sum() + self.beta * (self.params**2).sum()
+
+    def fit(self, loader: DataLoader, optimizer=None, loss_function=None) -> None:
+        """
+        I should really be using a DataLoader here, to handle minibatching elegantly, 
+            rather than using X, y like sklearn.
+
+        Fits the model to the data.
+        If no optimizer is passed in, the default optimizer is SGD.
+        If no loss function is passed in, the default loss function is MSE.
+        :returns: None; self.params are fit to the data.
+        """
+        if optimizer is None:
+            optimizer = SGD(0.01)
+
+        if loss_function is None:
+            loss_function = mean_squared_error
+
+        for X, y in loader:
+            if self.params is None:
+                self.params = Matrix([[Variable(random.random())] for _ in range(len(X[0]))])
+                self.bias = Matrix([[Variable(random.random())]])
+
+            output = self._evaluate(X)
+            loss = loss_function(output, y)
+            loss += self._regularize()
+            self.params = optimizer.step(self.params, loss.get_grad(self.params))
+            self.bias = optimizer.step(self.bias, loss.get_grad(self.bias))
+
+    def predict(self, X: Matrix) -> Matrix:
+        """
+        Given X, returns a list of predictions for X.
+        """
+        preds = []
+        for row in X:
+            preds.append(self._evaluate(Matrix([row]))[0])
+        return Matrix(preds)
+
---
+++ linear_models/logistic_regression.py
@@ -0,0 +1,73 @@
+import math
+
+from linear_regression import LinearRegression
+
+import sys
+sys.path.append('..')
+from autograd import Variable, Matrix
+from optimizers import SGD
+from dataloader import DataLoader
+
+
+def sigmoid(x: Variable) -> Variable:
+    return 1 / (1 + math.e**(-x))
+
+class LogisticRegression(LinearRegression):
+    """
+    Implements logistic regression.
+    That is, we assume that the manifold of the data is a plane with a pointwise application of
+        the sigmoid function.
+    """
+    def __init__(self, params=None, bias=None, alpha=0, beta=0):
+        """
+        :param params: The starting parameters to use.
+        """
+        super().__init__(params, bias, alpha, beta)
+
+    def _evaluate(self, row: Matrix) -> Matrix:
+        """
+        Uses the _evaluate function in LinearRegression, then applies a sigmoid function
+            elementwise to the resulting matrix.
+        """
+        mat = super()._evaluate(row).elementwise_apply(sigmoid)
+        return mat
+
+
+if __name__ == "__main__":
+    import random
+
+    from loss_functions import binary_cross_entropy
+
+    params = [0.3, 0.6, 0.2]
+    f = lambda row: sigmoid(sum([row[i] * params[i] for i in range(len(row))]) + 0.2)
+    X = []
+    y = []
+    thresh = 0.3
+    for _ in range(2000):
+        X.append([random.random() for _ in range(3)])
+        val = f(X[-1])
+        y.append([val > thresh])
+
+    loader = DataLoader(X, y)
+
+    logreg = LogisticRegression()
+    optim = SGD(0.01, minibatch_size=5)
+    logreg.fit(loader)
+    print('params')
+    print(logreg.params)
+    print(logreg.bias)
+    print('bce')
+    print(binary_cross_entropy(logreg.predict(Matrix(X)), Matrix(y)))
+
+    X_test = []
+    for _ in range(10):
+        X_test.append([random.random() for _ in range(3)])
+
+    outputs = logreg.predict(Matrix(X_test))
+    res = []
+    for i, output in enumerate(outputs):
+        res.append([f(X_test[i]) > thresh])
+        print(output, f(X_test[i]) > thresh)
+
+
+    print(binary_cross_entropy(logreg.predict(Matrix(X_test)), Matrix(res)))
