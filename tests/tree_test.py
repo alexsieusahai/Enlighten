3979fe94533d52810b510411e14ced5727a776f6
alexsieusahai <alexsieu14@gmail.com>
2019-04-03 18:55:17 -0600

    Revamp tree test

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -1,4 +1,4 @@
-from .generate_linear_dataset import generate_linear_dataset
+import numpy as np
 
 from ..autograd import Variable, Matrix
 from ..tree_models import Node, MajorityClassifier, negative_accuracy
@@ -7,10 +7,18 @@ from ..optimizers import SGD
 
 
 def test_tree():
+    num_cols, num_samples = 10, 10000
+    X = []
+    y = []
+
+    for _ in range(num_samples):
+        target = np.random.randint(0, 2)
+        X.append([np.random.random() for _ in range(num_cols-1)] + [target])
+        y.append([target])
+
+    loader = DataLoader(X, y)
 
     tree = Node(negative_accuracy, MajorityClassifier, {}, num_splits=20, min_samples=10)
-    num_cols, num_samples = 3, 10000
-    loader, true_params = generate_linear_dataset(num_cols, num_samples)
 
     for i in range(len(loader.y)):
         loader.y[i] = [loader.y[i][0] > 4]
@@ -19,4 +27,4 @@ def test_tree():
 
     preds = tree.predict(loader.X)
 
-    assert -1 * negative_accuracy(preds, loader.y) > 0.5
+    assert -1 * negative_accuracy(preds, loader.y) == 1
